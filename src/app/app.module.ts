470a45494a26b15ece3f5d5bb3148d2ff754f394
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2022-02-21 18:57:13 +0530

    example added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -13,9 +14,10 @@ import { counterReducer } from './counter.reducer';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     StoreModule.forRoot({count: counterReducer}, {})
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
